c1332741d53d31a95a3e5206c96eaeade9714225
819750348 <819750348@qq.com>
2019-09-18 07:58:47 +0800

    首页，节点，日志

---
+++ ant-design-jantd-vue/src/views/modules/iservice/NodeManager.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="components-layout-demo-basic">
+    <a-row type="flex" justify="center">
+      <a-col :span="8">
+        <a-input-search placeholder="搜索你想要的节点" @search="onSearch" enterButton="搜索" size="smill"/>
+      </a-col>
+    </a-row>
+    <a-layout style="margin-top: 10px">
+      <a-table :dataSource="data" :fit="true">
+        <a-table-column min-width="140px" title="节点编号">
+          <template slot-scope="scope">
+            {{scope.name}}
+          </template>
+        </a-table-column>
+        <a-table-column min-width="140px" title="主机名">
+          <template slot-scope="scope">
+            {{scope.name}}
+          </template>
+        </a-table-column>
+        <a-table-column min-width="140px" title="IP">
+          <template slot-scope="scope">
+            {{scope.name}}
+          </template>
+        </a-table-column>
+        <a-table-column min-width="140px" title="部署服务数">
+          <template slot-scope="scope">
+            {{scope.name}}
+          </template>
+        </a-table-column>
+        <a-table-column min-width="140px" title="运行服务数">
+          <template slot-scope="scope">
+            {{scope.name}}
+          </template>
+        </a-table-column>
+        <a-table-column min-width="140px" title="内存占用率">
+          <template slot-scope="scope">
+            <a-progress :percent="scope.num" :showInfo="false" />
+          </template>
+        </a-table-column>
+        <a-table-column min-width="140px" title="CPU占用率">
+          <template slot-scope="scope">
+            <a-progress :percent="scope.num" :showInfo="false" />
+          </template>
+        </a-table-column>
+      </a-table>
+    </a-layout>
+  </div>
+</template>
+<script>
+  import ATableColumn from "ant-design-vue/es/table/Column";
+  import ARow from "ant-design-vue/es/grid/Row";
+
+  const data = [{
+    key: '1',
+    name: 'John Brown',
+    num: '21',
+    address: 'New York No. 1 Lake Park',
+  }, {
+    key: '2',
+    name: 'Jim Green',
+    num: '44',
+    address: 'London No. 1 Lake Park',
+  }, {
+    key: '3',
+    name: 'Joe Black',
+    num: '66',
+    address: 'Sidney No. 1 Lake Park',
+  }];
+
+
+  export default {
+    data() {
+      return {
+        data
+      }
+    },
+    components: {ARow, ATableColumn}
+  }
+</script>
+<style>
+  #components-layout-demo-basic {
+    text-align: center;
+    background-color: #fff;
+    padding: 10px;
+  }
+
+  #components-layout-demo-basic .ant-layout-header,
+  #components-layout-demo-basic .ant-layout-footer {
+    background: #fff;
+    color: #fff;
+  }
+
+  #components-layout-demo-basic .ant-layout-footer {
+    line-height: 1.5;
+  }
+
+  #components-layout-demo-basic .ant-layout-sider {
+    background: #fff;
+    color: #fff;
+    /*line-height: 120px;*/
+  }
+
+  #components-layout-demo-basic .ant-layout-content {
+    background: #fff;
+    color: #fff;
+    min-height: 120px;
+    line-height: 120px;
+  }
+
+  #components-layout-demo-basic > .ant-layout {
+    margin-bottom: 48px;
+  }
+
+  #components-layout-demo-basic > .ant-layout:last-child {
+    margin: 0;
+  }
+</style>
---
+++ ant-design-jantd-vue/src/views/modules/iservice/ServiceLog.vue
@@ -0,0 +1,268 @@
+<template>
+  <div>
+    <a-row type="flex" justify="center">
+      <a-col :span="6" offset="6">
+        <a-input-search placeholder="搜索你想的日志" @search="onSearch" enterButton="搜索" size="smill"/>
+      </a-col>
+      <a-col :span="6">
+        <a-button size="medium" label="right" @click="addService" icon="el-icon-download" style="float: right">下载日志文件
+        </a-button>
+      </a-col>
+    </a-row>
+    <div v-if="contentArray.length > 0">
+      <div v-for="(item,i) in contentArray">
+        <div :class="showTotal ? 'total-introduce' : 'detailed-introduce'">
+          <div class="intro-content" :title="item.content" ref="desc">
+        <span class="merchant-desc" v-if="item">
+          {{item.content}}
+        </span>
+            <div class="unfold" @click="showTotalIntro" v-if="showExchangeButton">
+              <p>{{exchangeButton ? '展开' : '收起'}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Spread',
+    data() {
+      return {
+        title: '演示展开收起',
+        introduce: '',
+        // 是否展示所有文本内容
+        showTotal: true,
+        // 显示展开还是收起
+        exchangeButton: true,
+        // 是否显示展开收起按钮
+        showExchangeButton: false,
+        rem: '',
+        contentArray: [{ content:'拥有财富、11111111111111111111111111111111111111111111111111'+
+            '11222润444444444444444反反复复风付付付付付付付付付付付付付柔柔弱弱若若若若' +
+          '若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若' +
+          '若若若付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付' +
+          '付付付付付付名声、声、名声、式框架。Vue拥名声、式框架。Vue拥有财富、名声、式框架。Vue拥有' +
+          '财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、' +
+          '名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框' +
+          '架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富' +
+          '、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。V' +
+          'ue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、' +
+          '式框架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。VueVueVue'
+
+    },{  content:'拥有财富、1111111111111111111111111111111111111111111111111111222润4444444' +
+            '44444444反反复复风付付付付付付付付付付付付付柔柔弱弱若若若若若若若若若若若若' +
+          '若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若付付付付付' +
+          '付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付名声、' +
+          '声、名声、式框架。Vue拥名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。V' +
+          'ue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声' +
+          '、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财' +
+          '富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥' +
+          '有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vu' +
+          'e拥有财富、名声、式框架。V' +
+          'ue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。拥有财富、名ue拥有财富、名' +
+          '声、式框架。Vue拥有财富、名声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声' +
+          '、式框架。Vueue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。拥有财富、名声、' +
+          '式框架。Vue拥有财富、名声、式框架。Vueue拥有财富、名声、式框架。Vue拥有财富、名声、式框' +
+          '架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vueue拥有财富、名声、式框架。Vue拥' +
+          '有财富、名声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vueue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vueue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vueue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vueue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vueue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vueue拥有财富、名声、式框架。Vue拥有财富、名' +
+          '声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声' +
+          '、式框架。Vue声、式框架。Vue拥有财富、名声、式框架。Vue' +
+          'VueVue'
+    }]
+      };
+    },
+    mounted() {
+      this.init();
+    },
+    methods: {
+      showTotalIntro() {
+        console.log(this.showTotal);
+        this.showTotal = !this.showTotal;
+        this.exchangeButton = !this.exchangeButton;
+      },
+      getRem() {
+        console.log('getRem');
+        const defaultRem = 16;
+        let winWidth = window.innerWidth;
+        console.log('winWidth:' + winWidth);
+        let rem = winWidth / 1225 * defaultRem;
+        return rem;
+      },
+      init() {
+        this.introduce = '拥有财富、1111111111111111111111111111111111111111111111111111222润444444444444444反反复复风付付付付付付付付付付付付付柔柔弱弱若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若若付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付付名声、声、名声、式框架。Vue拥名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。Vue拥有财富、名声、式框架。拥有财富、名声、式框架。Vue拥有财富、名声、式框架。VueVueVue';
+      },
+    },
+    watch: {
+      contentArray: {
+        hander (val) {
+          this.$nextTick(function () {
+            console.log('nextTick');
+            // 判断介绍是否超过四行
+            let rem = parseFloat(this.getRem());
+            console.log('watch 中的rem', rem);
+            if (!this.$refs.desc) {
+              console.log('desc null');
+              return;
+            }
+            let descHeight = window.getComputedStyle(this.$refs.desc).height.replace('px', '');
+            console.log('descHeight:' + descHeight);
+            console.log('如果 descHeight 超过' + (5.25 * rem) + '就要显示展开按钮');
+            if (descHeight > 5.25 * rem) {
+              console.log('超过了四行');
+              // 显示展开收起按钮
+              this.showExchangeButton = true;
+              this.exchangeButton = true;
+              // 不是显示所有
+              this.showTotal = false;
+            } else {
+              // 不显示展开收起按钮
+              this.showExchangeButton = false;
+              // 没有超过四行就显示所有
+              this.showTotal = true;
+              console.log('showExchangeButton', this.showExchangeButton);
+              console.log('showTotal', this.showTotal);
+            }
+          }.bind(this));
+        },
+        deep: true
+      }
+
+    }
+  };
+</script>
+
+<style lang="less" scoped rel="stylesheet/less">
+  .top-prove {
+    height: 100px;
+    width: 100%;
+    background: #dddddd;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .total-introduce {
+    height: auto;
+    overflow: hidden;
+    font-size: 13px;
+    color: #434343;
+    margin: 10px;
+    .intro-content {
+      background: #ffffff;
+      .merchant-desc {
+        width: 100%;
+        line-height: 21px;
+      }
+    }
+    .unfold {
+      display: block;
+      z-index: 11;
+      float: right;
+      width: 40px;
+      height: 21px;
+      p {
+        margin: 0;
+        line-height: 21px;
+        color: #7fbe87;
+      }
+    }
+  }
+
+  .detailed-introduce {
+    font-size: 13px;
+    color: #434343;
+    position: relative;
+    overflow: hidden;
+    margin: 10px;
+    .intro-content {
+      // 最大高度设为4倍的行间距
+      max-height: 84px;
+      line-height: 21px;
+      word-wrap: break-word;
+      /*强制打散字符*/
+      word-break: break-all;
+      background: #ffffff;
+      /*同背景色*/
+      color: #ffffff;
+      overflow: hidden;
+      .merchant-desc {
+        width: 100%;
+        line-height: 21px;
+      }
+      &:after,
+        // 这是展开前实际显示的内容
+      &:before {
+        content: attr(title);
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        color: #434343
+        // overflow: hidden;
+      }
+      // 把最后最后一行自身的上面三行遮住
+      &:before {
+        display: block;
+        overflow: hidden;
+        z-index: 1;
+        max-height: 63px;
+        background: #ffffff;
+      }
+      &:after {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        height: 81px;
+        /*截取行数*/
+        -webkit-line-clamp: 4;
+        text-overflow: ellipsis;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        /*行首缩进字符数，值为[(截取行数-1)*尾部留空字符数]*/
+        text-indent: -12em;
+        /*尾部留空字符数*/
+        padding-right: 4em;
+      }
+      .unfold {
+        z-index: 11;
+        width: 40px;
+        height: 21px;
+        outline: 0;
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        p {
+          margin: 0;
+          line-height: 21px;
+          color: #7fbe87;
+        }
+      }
+    }
+  }
+
+  .bottom-prove {
+    height: 100px;
+    width: 100%;
+    background: #dddddd;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .change-buttom {
+    font-size: 14px;
+    color: #2371be;
+    .long {
+      text-align: right;
+      text-align: center;
+      height: 21px;
+    }
+    .short {
+      text-align: right;
+      text-align: center;
+      height: 20px;
+    }
+  }
+</style>
