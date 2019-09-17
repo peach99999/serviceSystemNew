f361e9eb80918dea7c3edbfd68f11e493aaaf161
819750348 <819750348@qq.com>
2019-09-17 09:16:15 +0800

    修改菜单树

--- ant-design-jantd-vue/src/views/modules/iservice/ServiceManager.vue
+++ ant-design-jantd-vue/src/views/modules/iservice/ServiceManager.vue
@@ -7,35 +7,13 @@
     </a-row>
     <a-layout style="margin-top: 10px">
       <a-layout-sider>
-        <a-menu
-          mode="inline"
-          :openKeys="openKeys"
-          @openChange="onOpenChange"
+        <a-tree
+          showLine
+          :treeData="treeData"
+          @select="this.onSelect"
+          @check="this.onCheck"
         >
-          <a-sub-menu key="sub1">
-            <span slot="title"><a-icon type="mail"/><span>Navigation One</span></span>
-            <a-menu-item key="1">Option 1</a-menu-item>
-            <a-menu-item key="2">Option 2</a-menu-item>
-            <a-menu-item key="3">Option 3</a-menu-item>
-            <a-menu-item key="4">Option 4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <span slot="title"><a-icon type="appstore"/><span>Navigation Two</span></span>
-            <a-menu-item key="5">Option 5</a-menu-item>
-            <a-menu-item key="6">Option 6</a-menu-item>
-            <a-sub-menu key="sub3" title="Submenu">
-              <a-menu-item key="7">Option 7</a-menu-item>
-              <a-menu-item key="8">Option 8</a-menu-item>
-            </a-sub-menu>
-          </a-sub-menu>
-          <a-sub-menu key="sub4">
-            <span slot="title"><a-icon type="setting"/><span>Navigation Three</span></span>
-            <a-menu-item key="9">Option 9</a-menu-item>
-            <a-menu-item key="10">Option 10</a-menu-item>
-            <a-menu-item key="11">Option 11</a-menu-item>
-            <a-menu-item key="12">Option 12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
+        </a-tree>
       </a-layout-sider>
       <a-layout>
         <a-layout-content>
@@ -94,11 +72,67 @@
 <script>
   import ATableColumn from "ant-design-vue/es/table/Column";
   import ARow from "ant-design-vue/es/grid/Row";
-
+  const treeData = [{
+    title: 'parent 1',
+    key: '0-0',
+    children: [{
+      title: 'parent 1-0',
+      key: '0-0-0',
+      disabled: true,
+      children: [
+        { title: 'leaf', key: '0-0-0-0', disableCheckbox: true },
+        { title: 'leaf', key: '0-0-0-1' },
+      ],
+    }, {
+      title: 'parent 1-1',
+      key: '0-0-1',
+      children: [
+        { key: '0-0-1-0', slots: { title: 'title0010' }},
+      ],
+    }],
+  },
+    {
+      title: 'parent 1',
+      key: '0-0',
+      children: [{
+        title: 'parent 1-0',
+        key: '0-0-0',
+        disabled: true,
+        children: [
+          { title: 'leaf', key: '0-0-0-0', disableCheckbox: true },
+          { title: 'leaf', key: '0-0-0-1' },
+        ],
+      }, {
+        title: 'parent 1-1',
+        key: '0-0-1',
+        children: [
+          { key: '0-0-1-0', slots: { title: 'title0010' }},
+        ],
+      }],
+    },{
+      title: 'parent 1',
+      key: '0-0',
+      children: [{
+        title: 'parent 1-0',
+        key: '0-0-0',
+        disabled: true,
+        children: [
+          { title: 'leaf', key: '0-0-0-0', disableCheckbox: true },
+          { title: 'leaf', key: '0-0-0-1' },
+        ],
+      }, {
+        title: 'parent 1-1',
+        key: '0-0-1',
+        children: [
+          { key: '0-0-1-0', slots: { title: 'title0010' }},
+        ],
+      }],
+    }]
   export default {
     components: {ARow, ATableColumn},
     data() {
       return {
+        treeData,
         rootSubmenuKeys: ['sub1', 'sub2', 'sub4'],
         openKeys: ['sub1'],
         labelid: "",
